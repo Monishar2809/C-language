f9d77fa356dbd45bb72fed1fedf699dbd9376081
Monishar2809 <monishargowda2815@gmail.com>
2026-08-04 23:24:55 +0530

    area of isoceles triangle.c

---
+++ 01_Basics/area of isoceles triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,Area;
+    printf("enter a=");
+    scanf("%f",&a);
+    Area=1.0/2.0*pow(a,2);
+    printf("area=%f",Area);
+    return 0;
+}
