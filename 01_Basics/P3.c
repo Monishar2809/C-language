a17b77b708fcc1186239ec20d365b0dda7c97616
Monishar2809 <monishargowda2815@gmail.com>
2026-08-04 15:34:35 +0530

    P3.c

---
+++ 01_Basics/P3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,X;
+    printf("enter a=");
+    scanf("%d",&a);
+    printf("enter b=");
+    scanf("%d",&b);
+    printf("enter c=");
+    scanf("%d",&c);
+    printf("enter d=");
+    scanf("%d",&d);
+    X=(a+b)*(c+d);
+    printf("X=%d",X);
+    return 0;
+}
