7090bc398c55762fb65b0e5e9185c28059f52502
Monishar2809 <monishargowda2815@gmail.com>
2026-08-04 15:40:40 +0530

    P5.c

---
+++ 01_Basics/P5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int S,m,p;
+    printf("enter m=");
+    scanf("%d",&m);
+    printf("enter p=");
+    scanf("%d",&p);
+    S=(m+p)*p;
+    printf("S=%d",S);
+    return 0;
+}
