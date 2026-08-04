3ba37714ee7854c030c899c0bb5c7b6d4ac552f2
Monishar2809 <monishargowda2815@gmail.com>
2026-08-04 15:24:10 +0530

    add

---
+++ 01_Basics/SP=(x+y)+x.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,SP;
+    printf("enter x=");
+    scanf("%d",&x);
+    printf("enter y=");
+    scanf("%d",&y);
+    SP=(x+y)+x;
+    printf("SP=%d",SP);
+    return 0;
+}
