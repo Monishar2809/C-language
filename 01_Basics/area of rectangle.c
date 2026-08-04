a7b76d3a21fbce84573ef62c9d0ae6f4acbaa151
Monishar2809 <monishargowda2815@gmail.com>
2026-08-04 15:45:32 +0530

    area of rectangle.c

---
+++ 01_Basics/area of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float l,b,A;
+    printf("enter length=");
+    scanf("%f",&l);
+    printf("enter breadth=");
+    scanf("%f",&b);
+    A=l*b;
+    printf("area =%f",A);
+    return 0;
+}
