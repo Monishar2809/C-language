4c59eb91910fb77ee115753299b1cba5df682f96
Monishar2809 <monishargowda2815@gmail.com>
2026-08-07 04:03:17 +0530

    save

---
+++ 02_Conditional_statement/C_If_Else_Ladder_Statement/biggest of 3 numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the 3 numbers=");
+    scanf("%d,%d,%d",&a,&b,&c);
+    if((a>b)&&(a>c))
+    {
+        printf("%d is the biggest number",a);
+    }
+    else if((b>a)&&(b>c))
+    {
+        printf("%d is the biggest number",b);
+    }
+    else
+    {
+        printf("%d is the biggest number",c);
+    }
+    return 0;
+}
---
+++ 02_Conditional_statement/C_If_Else_Ladder_Statement/check p or n.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter number=");
+    scanf("%d",&x);
+    if(x>0)
+    {
+        printf("%d is positive number",x);
+    }
+    else if(x<0)
+    {
+        printf("%d is negative number",x);
+    }
+    else
+    {
+        printf("%d is zero",x);
+    }
+    return 0;
+}
